d0ab561771451ef88c787b1ef7a839cf1c31f29a
George Wilde <george@wildecreations.co.uk>
2019-09-24 08:06:06 +0100

    Allow Gatsby to access and show NetlifyCMS blog content

--- blog/this-is-a-test.md
+++ blog/this-is-a-test.md
@@ -1,6 +1,7 @@
 ---
-path: test
+path: blog/test
 date: 2019-09-23T19:41:26.959Z
 title: This is a test
 ---
+
 This is some test content!
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
